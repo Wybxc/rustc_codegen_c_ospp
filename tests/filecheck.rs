3ce0212f633287c08b8404a12f93e1bdaf556749
忘忧北萱草 <wybxc@stu.pku.edu.cn>
2024-07-11 18:16:39 +0800

    fix: FileCheck not found in actions
    Signed-off-by: 忘忧北萱草 <wybxc@stu.pku.edu.cn>

--- tests/filecheck.rs
+++ tests/filecheck.rs
@@ -1,12 +1,27 @@
 use std::fs::File;
 use std::process::Command;
 
+use which::which;
+
 const TESTCASES: &[&str] = &["filename"];
 
 #[test]
 pub fn filecheck() {
+    let make = which("make").expect("`make` not found");
+    let filecheck = [
+        "FileCheck",
+        "FileCheck-14",
+        "FileCheck-15",
+        "FileCheck-16",
+        "FileCheck-17",
+        "FileCheck-18",
+    ]
+    .into_iter()
+    .find_map(|filecheck| which(filecheck).ok())
+    .expect("`FileCheck` not found");
+
     for case in TESTCASES {
-        let output = Command::new("make").arg(format!("build/{case}")).output().unwrap();
+        let output = Command::new(&make).arg(format!("build/{case}")).output().unwrap();
         assert!(output.status.success(), "failed to build {case}");
 
         let generated = std::fs::read_dir("build")
@@ -17,7 +32,7 @@ pub fn filecheck() {
 
         let generated = generated.unwrap();
         let generated = File::open(generated.path()).unwrap();
-        let output = Command::new("FileCheck")
+        let output = Command::new(&filecheck)
             .arg(format!("example/tests/{case}.rs"))
             .stdin(generated)
             .output()
