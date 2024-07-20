5ce19fd69423533612e57a2d05074b70e31775a4
忘忧北萱草 <wybxc@stu.pku.edu.cn>
2024-07-21 00:34:55 +0800

    fix: int -> int32_t in test case

--- tests/codegen/params_count.rs
+++ tests/codegen/params_count.rs
@@ -5,7 +5,7 @@
 extern crate mini_core;
 
 // expect three int params
-// CHECK: {{((int .*,?\s?){3})}}
+// CHECK: {{((int32_t .*,?\s?){3})}}
 #[no_mangle]
 pub fn foo(_x: i32, _y: i32, _z: i32) -> i32 {
     0
